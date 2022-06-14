e5d4552a8668319579c6f326a2e3b72370d306dd
iamseanwhite <seanwhite211@gmail.com>
2022-06-14 02:30:36 -0400

    Add GetTime extension, observation plan, and visit files

--- oss_scripts/ope.js
+++ oss_scripts/ope.js
@@ -1,3 +1,20 @@
+//#!/usr/bin/env node
 const sp = require('../build/Release/scriptprocessor');
 
-console.log(sp.getTelemetry());
+var currentVisit;
+var observationPlan = [
+    {visit: "V001", begin: '2022-165/01:53:44', cutoff: '2022-165/02:55:44'},
+    {visit: "V002", begin: '2022-165/23:53:44', cutoff: '2022-165/23:53:44'}
+];
+
+var nextVisit = observationPlan[0];
+
+var systemTime = sp.getTime() * 1000;
+//TODO: convert to usable format
+var beginTime = Date.parse(nextVisit.begin);
+var cutoffTime = Date.parse(nextVisit.end);
+
+if (beginTime<= systemTime && systemTime < cutoffTime) {
+    console.log("Currently within visit window");
+}
+else console.log("Not currently within visit window");
--- scriptprocessor.cc
+++ scriptprocessor.cc
@@ -1,4 +1,6 @@
 #include <node.h>
+#include <chrono>
+#include <iostream>
 
 using namespace v8;
 
@@ -7,8 +9,17 @@ void GetTelemetry(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Returning telemetry...").ToLocalChecked());
 }
 
+//Retrieve system time as time since epoh in UTC
+void GetTime(const FunctionCallbackInfo<Value>& args) {
+  auto now = std::chrono::system_clock::now();
+  std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(Number::New(isolate, currentTime));
+}
+
 void Initialize(Local<Object> exports) {
   NODE_SET_METHOD(exports, "getTelemetry", GetTelemetry);
+  NODE_SET_METHOD(exports, "getTime", GetTime);
 }
 
 NODE_MODULE(scriptprocessor, Initialize)
